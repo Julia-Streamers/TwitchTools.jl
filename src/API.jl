870d05094678b0e5ace12ee3e701a338056d6c1a
WooKyoung Noh <wookay.noh@gmail.com>
2019-06-23 01:54:15 +0900

    debug_mode

--- src/API.jl
+++ src/API.jl
@@ -2,9 +2,16 @@ module API # TwitchTools
 
 using HTTP: HTTP
 
+env = Dict(
+    :debug_mode => true
+)
+
 function get_clip(client_id::String, clip_id::String)::Vector{UInt8}
     url = "https://api.twitch.tv/helix/clips?id=$clip_id"
     resp = HTTP.get(url, ["Client-ID"=>client_id])
+    if env[:debug_mode]
+        @info :get_clip (clip_id, length(resp.body))
+    end
     resp.body
 end
 
