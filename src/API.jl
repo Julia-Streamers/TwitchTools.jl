9ae4bc641fc55d3ddfe70abf2a4e81694a05dc8c
WooKyoung Noh <wookay.noh@gmail.com>
2019-06-23 01:11:12 +0900

    api

--- src/API.jl
+++ src/API.jl
@@ -4,7 +4,8 @@ using HTTP: HTTP
 
 function get_clip(client_id::String, clip_id::String)::String
     url = "https://api.twitch.tv/helix/clips?id=$clip_id"
-    HTTP.get(url, ["Client-ID"=>client_id])
+    resp = HTTP.get(url, ["Client-ID"=>client_id])
+    resp.body
 end
 
 end # TwitchTools.API
