135b7be7ca3493a4dae804cac45ffc2154c89982
WooKyoung Noh <wookay.noh@gmail.com>
2019-06-23 17:57:31 +0900

    get_clips_by_broadcaster

--- src/API.jl
+++ src/API.jl
@@ -15,4 +15,13 @@ function get_clip(client_id::String, clip_id::String)::Vector{UInt8}
     resp.body
 end
 
+function get_clips_by_broadcaster(client_id::String, broadcaster_id::Int)::Vector{UInt8}
+    url = "https://api.twitch.tv/helix/clips?broadcaster_id=$broadcaster_id"
+    resp = HTTP.get(url, ["Client-ID"=>client_id])
+    if env[:debug_mode]
+        @info :get_clips_by_broadcaster (broadcaster_id, length(resp.body))
+    end
+    resp.body
+end
+
 end # TwitchTools.API
--- test/twitchtools/api.jl
+++ test/twitchtools/api.jl
@@ -4,5 +4,6 @@ using Test
 using TwitchTools: API
 
 @test API.get_clip isa Function
+@test API.get_clips_by_broadcaster isa Function
 
 end # module test_twitchtools_api
