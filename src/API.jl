f0899bea69ac03610233be270ade85f4d3362365
WooKyoung Noh <wookay.noh@gmail.com>
2019-06-26 22:43:30 +0900

    refactor _get_request

--- src/API.jl
+++ src/API.jl
@@ -9,20 +9,24 @@ env = Dict(
 
 const RFC3339 = DateFormat("yyyy-mm-dd\\THH:MM:SSZ")
 
+function _get_request(name::Symbol, client_id::String, url::String, query::Dict{String,String})::Vector{UInt8}
+    resp = HTTP.get(url, ["Client-ID"=>client_id]; query=query)
+    if env[:debug_mode]
+        @info name, query, length(resp.body)
+    end
+    resp.body
+end
+
 function get_clip(client_id::String, clip_id::String)::Vector{UInt8}
     url = "https://api.twitch.tv/helix/clips"
     query = Dict{String,String}("id" => clip_id)
-    resp = HTTP.get(url, ["Client-ID"=>client_id]; query=query)
-    if env[:debug_mode]
-        @info :get_clip (clip_id, length(resp.body))
-    end
-    resp.body
+    _get_request(:get_clip, client_id, url, query)
 end
 
 function get_clips_by_broadcaster(client_id::String,
                                   broadcaster_id::Int,
                                   started_at::Union{Missing, Date, DateTime},
-                                  ended_at::Union{Missing, Date, DateTime}# started_at + 1 week
+                                  ended_at::Union{Missing, Date, DateTime} # started_at + 1 week
                                   )::Vector{UInt8}
     url = "https://api.twitch.tv/helix/clips"
     query = Dict{String,String}("broadcaster_id" => string(broadcaster_id))
@@ -32,11 +36,7 @@ function get_clips_by_broadcaster(client_id::String,
     if !ismissing(ended_at)
         query["ended_at"] = Dates.format(ended_at, RFC3339)
     end
-    resp = HTTP.get(url, ["Client-ID"=>client_id]; query=query)
-    if env[:debug_mode]
-        @info :get_clips_by_broadcaster (broadcaster_id, length(resp.body))
-    end
-    resp.body
+    _get_request(:get_clips_by_broadcaster, client_id, url, query)
 end
 
 end # TwitchTools.API
