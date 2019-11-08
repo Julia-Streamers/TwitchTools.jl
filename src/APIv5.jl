204dac234b4d5ec6510e523a443b2598b23f1c50
WooKyoung Noh <wookay.noh@gmail.com>
2019-11-08 22:50:01 +0900

    test apiv5

--- src/APIv5.jl
+++ src/APIv5.jl
@@ -3,13 +3,12 @@ module APIv5 # TwitchTools
 using HTTP: HTTP
 
 env = Dict(
-    :debug_mode => true
+    :debug_mode => false
 )
 
 function get_clip(client_id::String,
                   clip_id::String;
                   url = "https://api.twitch.tv/kraken/clips/$clip_id")::Vector{UInt8}
-    url = "https://api.twitch.tv/kraken/clips/$clip_id"
     resp = HTTP.get(url, ["Client-ID"=>client_id, "Accept"=>"application/vnd.twitchtv.v5+json"])
     if env[:debug_mode]
         @info :get_clip (clip_id, length(resp.body))
--- test/twitchtools/api.jl
+++ test/twitchtools/api.jl
@@ -1,10 +1,11 @@
 module test_twitchtools_api
 
 using Test
-using TwitchTools: API
+using TwitchTools: API, APIv5
 
 @test API.get_clip isa Function
 @test API.get_clips_by_broadcaster isa Function
+@test APIv5.get_clip isa Function
 
 using Bukdu
 struct ClipController <: ApplicationController
@@ -17,6 +18,7 @@ end
 
 routes() do
     get("/clips", ClipController, clips)
+    get("/clips/:clipid", ClipController, clips)
 end
 
 Bukdu.start(8080)
@@ -24,6 +26,9 @@ Bukdu.start(8080)
 API.get_clip("client_id", "clip_id"; url="http://localhost:8080/clips")
 broadcaster_id = 0
 API.get_clips_by_broadcaster("client_id", broadcaster_id, nothing, nothing; url="http://localhost:8080/clips")
+
+clip_id = "clip_id"
+APIv5.get_clip("client_id", clip_id; url="http://localhost:8080/clips/$clip_id")
 
 Bukdu.stop()
 
