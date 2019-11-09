e9368092f941d6666e628a8acb0b719934823ce1
WooKyoung Noh <wookay.noh@gmail.com>
2019-11-09 17:49:09 +0900

    test api

--- test/twitchtools/api.jl
+++ test/twitchtools/api.jl
@@ -7,7 +7,8 @@ using TwitchTools: API, APIv5
 @test API.get_clips_by_broadcaster isa Function
 @test APIv5.get_clip isa Function
 
-using Bukdu
+using Bukdu, HTTP
+
 struct ClipController <: ApplicationController
     conn::Conn
 end
@@ -21,17 +22,6 @@ routes() do
     get("/kraken/clips/:clipid", ClipController, clips)
 end
 
-Bukdu.start(8080)
-
-using HTTP
-const local_api_server = HTTP.URI("http://localhost:8080/")
-
-API.get_clip("client_id", "clip_id"; server=local_api_server)
-API.get_clips_by_broadcaster("client_id", 0, nothing, nothing; server=local_api_server)
-APIv5.get_clip("client_id", "clip_id"; server=local_api_server)
-
-Bukdu.stop()
-
 function mock_action(server, path, headers=[], query=nothing)
     if query !== nothing
         path = string(merge(HTTP.URI(path), query=query))
@@ -42,5 +32,16 @@ end
 API.get_clip("client_id", "clip_id"; action=mock_action)
 API.get_clips_by_broadcaster("client_id", 0, nothing, nothing; action=mock_action)
 APIv5.get_clip("client_id", "clip_id"; action=mock_action)
+
+
+Bukdu.start(8080)
+
+const local_api_server = HTTP.URI("http://localhost:8080/")
+
+API.get_clip("client_id", "clip_id"; server=local_api_server)
+API.get_clips_by_broadcaster("client_id", 0, nothing, nothing; server=local_api_server)
+APIv5.get_clip("client_id", "clip_id"; server=local_api_server)
+
+Bukdu.stop()
 
 end # module test_twitchtools_api
